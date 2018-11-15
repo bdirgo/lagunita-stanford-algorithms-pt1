fb512071c43ac1ef9eb653d848aa97327b5d5921
Benjamin Dirgo <benjamin.dirgo@gm.com>
2018-11-15 13:45:41 -0700

    Finished HW1! Turns out I wasn't checking the length correctly when recursivly calling. And this caused non-sorted two element arrays to be merged in to the output These arrays should have been split into two one element arrays instead.

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,24 +1,22 @@
 import math
 
-# file = open('integerArray.txt', 'r')
-file = open('test.txt', 'r')
+file = open('integerArray.txt', 'r')
+# file = open('test.txt', 'r')
 listOfNums = []
 for line in file:
 	listOfNums.append(int(line))
 
 def mergeAndCountSplitInv(B, C, D):
-
 	i = 0
 	j = 0
 	total = 0
 	b = len(B)
 	c = len(C)
-	print("B", B)
-	print("C", C)
-	for k in range(len(D)):
-		# print("i", i)
-		# print("j", j)
-
+	d = len(D)
+	# print("B (mcs)", B)
+	# print("C (mcs)", C)
+	# print("D (mcs)", D)
+	for k in range(d):
 		if i == b:
 			D[k] = C[j]
 			j = j + 1
@@ -32,30 +30,33 @@ def mergeAndCountSplitInv(B, C, D):
 			D[k] = C[j]
 			j = j + 1
 			total += len(B[i:])
-		print("D[k]", D[k])
+	# 	print("D[k]", D[k])
 
-	print("D", D)
-	print("total", total)
+	# print("D (mcs)", D)
+	# print("total", total)
 
 	return total
 
+def split(inputList):
+	iLen = len(inputList)
+	midpoint = iLen // 2
+	return inputList[:midpoint], inputList[midpoint:]
 
-def sortAndCount(A, n):
-	print("A", A)
-	l = math.floor(n / 2)
-	if n == 1:
+
+def sortAndCount(A):
+	# print("A", A)
+	n = len(A)
+	if n <= 1:
 		return 0
 	else:
-		B = A[:l]
-		C = A[l:]
+		B, C = split(A)
 		D = A
-		print("B", B)
-		print("C", C)
-
-		x = sortAndCount(B, l)
-		y = sortAndCount(C, l)
+		# print("B (sc)", B)
+		x = sortAndCount(B)
+		# print("C (sc)", C)
+		y = sortAndCount(C)
 		z = mergeAndCountSplitInv(B, C, D)
-		print("D", D)
+		# print("D (sc)", D)
 	return x + y + z
 
-print(sortAndCount(listOfNums, len(listOfNums)))
+print(sortAndCount(listOfNums))
